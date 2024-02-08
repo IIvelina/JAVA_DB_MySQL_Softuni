ad758198ebe37bd6d3398b6cf5e48705beb51fdd
Ivelina Ivanova <125279068+IIvelina@users.noreply.github.com>
2024-02-08 21:25:31 +0200

    Add files via upload

---
+++ Database Programmability/Exercise/2. Employees with Salary Above Number.sql
@@ -0,0 +1,8 @@
+
+DELIMITER $$
+CREATE PROCEDURE usp_get_employees_salary_above(desired_salary DECIMAL(19,4))
+BEGIN 
+	SELECT first_name, last_name FROM employees
+    WHERE salary >= desired_salary
+    ORDER BY first_name, last_name, employee_id;
+END$$
---
+++ Database Programmability/Exercise/3. Town Names Starting With.sql
@@ -0,0 +1,8 @@
+
+DELIMITER $$
+CREATE PROCEDURE usp_get_towns_starting_with (town_substring VARCHAR(50))
+BEGIN 
+	SELECT name AS town_name FROM towns
+    WHERE name LIKE CONCAT(town_substring, '%')
+    ORDER BY name;
+END$$
